581f5f6613171cae7d7bdc1591fed3c28b993b3b
Minh <congminh292k@gmail.com>
2023-03-02 17:44:19 +0700

    fix version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="ssh-pocket",
-    version="1.2.3",
+    version="1.2.2",
     author="Dawn",
     author_email="congminh292k@gmail.com",
     description="Easy peasy ssh choosey",
